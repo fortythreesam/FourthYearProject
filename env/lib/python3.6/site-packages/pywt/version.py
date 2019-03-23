317393bd3c78130b3bdcaf6e3747c9fd458646b5
fortythreesam <samdrugan@gmail.com>
2019-03-23 21:35:46 +0000

    added virtual environment

---
+++ env/lib/python3.6/site-packages/pywt/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM PYWAVELETS SETUP.PY
+short_version = '1.0.2'
+version = '1.0.2'
+full_version = '1.0.2'
+git_revision = '21a30d2af5aca2b3c5f827aa407cb549e2c99fb9'
+release = True
+
+if not release:
+    version = full_version
